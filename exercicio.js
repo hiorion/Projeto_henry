92ff2c5ab95ae19e28a4d9fd72eda8970c0dd7ca
henry orion <hen.orion@gmail.com>
2025-05-29 00:49:42 -0300

    fisrt update

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+// Função de multiplicação com tipagem
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação com tipagem
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso
+console.log("Resultado da multiplicação:", multiplicar(5, 3));
+console.log(saudacao("Henry"));
---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação com tipagem
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+// Função de saudação com tipagem
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+// Exemplos de uso
+console.log("Resultado da multiplicação:", multiplicar(5, 3));
+console.log(saudacao("Henry"));
